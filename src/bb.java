2af79bf3cab98da55b7d883d0e20d2515ab4f7ac
EfremDebretsion <46765782+EfremDebretsion@users.noreply.github.com>
2019-01-30 22:04:27 -0800

    add multiply method

--- src/bb.java
+++ src/bb.java
@@ -6,4 +6,7 @@ public class bb {
     public static double subtact( double n1, double n2){
         return  n1 - n2;
     }
+    public static double multiply( double n1, double n2){
+        return  n1 * n2;
+    } 
 }
