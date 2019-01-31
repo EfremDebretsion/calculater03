4e931c6f12a299e80184f513d5acca1dfed64abc
selamey <efrem.tsehaye2002@gmail.com>
2019-01-30 21:57:31 -0800

    subtract

--- src/bb.java
+++ src/bb.java
@@ -3,4 +3,7 @@ public class bb {
     public static double add(double n1 , double n2){
         return n1 + n2;
     }
+    public static double subtact( double n1, double n2){
+        return  n1 - n2;
+    }
 }
